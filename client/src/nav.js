d40ce1746126e737bc5a5636c321cd34658a004b
Annique Nsabimana <anniquensabimana@gmail.com>
2019-09-16 18:54:28 -0500

    Stretch Complete

---
+++ client/src/nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu} from "semantic-ui-react";
+
+const Nav = (props) => {
+  const logout = (e) => {
+      e.preventDefault();
+      localStorage.removeItem('token');
+      props.history.push('/')
+  }
+  return (
+    <Menu>
+      <Menu.Item as={Link} to={"/"}>
+        Home
+      </Menu.Item>
+      <Menu.Item as={Link} to={"/userlist"}>
+        Userlist
+      </Menu.Item>
+      <Menu.Item onClick={logout} to={"/"}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default Nav;
---
+++ client/src/userlist.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card } from 'semantic-ui-react';
+
+const UserList = () => {
+  const [users, setUsers] = useState();
+  const token = localStorage.getItem('token')
+
+  // this should be recoded to get a token from local or session storage, 
+  // and make GET request on /users
+
+  useEffect(() => {
+    token &&
+    axios
+      .get("http://localhost:5001/api/usernames")
+      .then(response => {
+        // console.log(response);
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+      <div>
+          {users && users.map(user => <Card color='green'>{user.username}</Card>)}
+      </div>
+  )
+};
+
+export default UserList;
